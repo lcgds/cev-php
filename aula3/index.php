0e769194fb852f2f4acda180ade6858e034e3f23
LCGDS <lcgds@live.com>
2020-12-05 08:41:06 -0300

    + Aula 2

---
+++ aula3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Curso de PHP - CursoemVideo.com</title>
+    <style>
+        h1 {
+            color: blue;
+        }
+
+        h2 {
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Testando PHP</h1>
+
+    <?php 
+        echo "<h2>Olá, Mundo!<h2>";
+    ?>
+
+</body>
+
+</html>
